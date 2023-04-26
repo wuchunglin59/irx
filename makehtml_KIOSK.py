beb06c19191c8cfe20e58d9dde4026ef63455b73
wuchunglin <wuchunglin59@gmail.com>
2023-04-26 16:17:01 +0800

    add ibx irx kiosk圖檔

--- makehtml_KIOSK.py
+++ makehtml_KIOSK.py
@@ -2,51 +2,44 @@ import os
 import re
 
 
-path = 'images\\KIOSK'
+'''
+html = ''
+file = open('html_model.txt', 'r', encoding='UTF-8')
+for line in file.readlines():
+  html += line
+file.close()
+
 files = os.listdir(path)
-
-
-dirList = []
-for dirName in [x[0] for x in os.walk(path)]:
-  #print(dirName)
-  isdir = False
-  for item in os.listdir(dirName):
-    if os.path.isdir(dirName + '\\' + item):
-      isdir = True
-      break
-  if not isdir:
-    dirList.append(dirName)
-
-
-for d in dirList:
-  fileName = '_'.join(d.split('\\')[2:])
-  print(fileName)
-  png_list = os.listdir(d)
+for dir_name in files:
+  png_list = os.listdir(path + '/' + dir_name)
+  png_html = ''
+  for png_name in png_list:
+    if '.png' in png_name:
+      png_html += '<li><img src="' + path + '/' + dir_name + '/' + png_name + '" alt=""></li>' + '\n'
+  file = open('images_html/IRX/' + dir_name + '.html' ,'w', encoding='UTF-8')
+  file.write(html.replace('message',png_html))
+  file.close()
+'''
+'''舊版本
+files = os.listdir(path)
+for dir_name in files:
+  png_list = os.listdir(path + '/' + dir_name)
+  png_html = ''
+  for png_name in png_list:
+    if '.png' in png_name:
+      png_html += '<li><img src="' + path + '/' + dir_name + '/' + png_name + '"></li>' + '\n'
+  file = open('images_html/IRXM/' + dir_name + '.txt' ,'w', encoding='UTF-8')
+  file.write('<ul class="slides">' + png_html + '</ul>')
+  file.close()
+'''
+path = 'images/KIOSK'
+files = os.listdir(path)
+for dir_name in files:
+  png_list = os.listdir(path + '/' + dir_name)
+  
   carousel_indicators = '<ul class="carousel-indicators carousel-indicators-numbers">' + '\n'
   carousel_inner = ''
   i = 0
-  for png_name in png_list:
-
-    if re.search('([-\w]+\.(?:jpg|jpeg|gif|png))', png_name.lower()):
-        carousel_indicators += '<li data-target="#myCarousel" data-slide-to="' + str(i) + '">' + str(i+1) + '</li>' + '\n'
-        carousel_inner += '<div class="carousel-item"><img src="' + d + '\\' + png_name + '" alt=""></div>' + '\n'
-        i += 1
-        
-  file = open('images_html/KIOSK/' + fileName + '_indicators.txt' ,'w', encoding='UTF-8')
-  file.write(carousel_indicators + "</ul>")
-  file.close()
-  file = open('images_html/KIOSK/' + fileName + '_inner.txt' ,'w', encoding='UTF-8')
-  file.write(carousel_inner)
-  file.close()
-    
-
-'''
-for dir_name in files:
-  png_list = os.listdir(path + '/' + dir_name)
-
-  carousel_indicators = '<ul class="carousel-indicators carousel-indicators-numbers">' + '\n'
-  carousel_inner = '';
-  i = 0;
   for png_name in png_list:
     if re.search('([-\w]+\.(?:jpg|jpeg|gif|png))', png_name.lower()):
         carousel_indicators += '<li data-target="#myCarousel" data-slide-to="' + str(i) + '">' + str(i+1) + '</li>' + '\n'
@@ -58,6 +51,7 @@ for dir_name in files:
   file.close()
   file = open('images_html/KIOSK/' + dir_name + '_inner.txt' ,'w', encoding='UTF-8')
   file.write(carousel_inner)
-  file.close()
-  '''
+  file.close()  
 print('end')
+    
+
